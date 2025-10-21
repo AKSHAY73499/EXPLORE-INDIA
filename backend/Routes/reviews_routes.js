b6d57e659c9e39b50bf2ecb9b59ff6cbe59dd20d
Akshay Kumar <akshaym73499@gmail.com>
2025-10-21 16:04:04 +0530

    Delete backend directory

--- backend/Routes/reviews_routes.js
+++
@@ -1,9 +0,0 @@
-const express =require("express");
-const { createReview } = require("../controllers/review_controller");
-const {verifyUser}=require('../utils/verifyToken');
-
-const router =express.Router()
-
-router.post('/:tourId',createReview)
-
-module.exports = router;
